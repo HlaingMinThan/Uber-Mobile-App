d98e016b1d5af9da627d6181f6db21c19724e0b7
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-09-26 17:15:33 +0700

    starting new pages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout() {
 
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerTitle: "Home" }} />
       <Stack.Screen name="+not-found" />
     </Stack>
   );
---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, SafeAreaView } from "react-native";
+import React from "react";
+
+const index = () => {
+  return (
+    <SafeAreaView>
+      <Text className="text-3xl ">index</Text>
+    </SafeAreaView>
+  );
+};
+
+export default index;
