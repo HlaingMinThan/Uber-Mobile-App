80335a6535dc63f62e1da059cfcb5f6f2b157645
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-10-24 20:18:59 +0700

    basic swiper

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,31 @@
-import { Text, SafeAreaView } from "react-native";
-import React from "react";
+import { Text, SafeAreaView, TouchableOpacity, View } from "react-native";
+import React, { useRef } from "react";
+import Swiper from "react-native-swiper";
+import { data } from "@/constants/index";
 
-const index = () => {
+const Index = () => {
+  const swiperRef = useRef<Swiper>(null);
+
   return (
-    <SafeAreaView>
-      <Text className="text-3xl ">index</Text>
+    <SafeAreaView className="flex h-full justify-between items-center">
+      <TouchableOpacity className="w-full items-end p-5">
+        <Text className="text-black font-JakartaBold text-md">Skip</Text>
+      </TouchableOpacity>
+
+      <Swiper ref={swiperRef}>
+        {data.onboarding.map((item: any) => (
+          <View
+            key={item.id}
+            className="w-full h-full flex items-center justify-center"
+          >
+            <Text className="text-black font-JakartaBold text-2xl">
+              {item.title}
+            </Text>
+          </View>
+        ))}
+      </Swiper>
     </SafeAreaView>
   );
 };
 
-export default index;
+export default Index;
