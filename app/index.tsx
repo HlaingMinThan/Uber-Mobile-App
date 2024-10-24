8fd862e46327467520e9e412c0ca15495967c449
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-10-24 21:09:26 +0700

    custom helper button

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,14 @@
-import { Text, SafeAreaView, TouchableOpacity, View } from "react-native";
+import {
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  View,
+  Image,
+} from "react-native";
 import React, { useRef } from "react";
 import Swiper from "react-native-swiper";
 import { data } from "@/constants/index";
+import CustomButton from "@/components/CustomButton";
 
 const Index = () => {
   const swiperRef = useRef<Swiper>(null);
@@ -11,19 +18,38 @@ const Index = () => {
       <TouchableOpacity className="w-full items-end p-5">
         <Text className="text-black font-JakartaBold text-md">Skip</Text>
       </TouchableOpacity>
-
-      <Swiper ref={swiperRef}>
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={
+          <View className="w-[32px] h-[4px] mx-1  rounded-full bg-[#E2E8F0]"></View>
+        }
+        activeDot={
+          <View className="w-[32px] h-[4px] mx-1  rounded-full bg-[#02E8FF]"></View>
+        }
+      >
         {data.onboarding.map((item: any) => (
-          <View
-            key={item.id}
-            className="w-full h-full flex items-center justify-center"
-          >
-            <Text className="text-black font-JakartaBold text-2xl">
-              {item.title}
-            </Text>
+          <View key={item.id}>
+            <Image
+              source={item.image}
+              className="w-full h-[300px]"
+              resizeMode="contain"
+            ></Image>
+            <View className="w-full mt-10 flex items-center justify-center">
+              <Text className="text-black text-3xl font-bold mx-10 text-center">
+                {item.title} with{" "}
+                <Text className="text-primary-500 ">Ryde</Text>
+              </Text>
+              <Text className="mt-3 text-lg text-[#858585] font-JakartaSemiBold font-bold mx-10 text-center">
+                {item.description}
+              </Text>
+            </View>
           </View>
         ))}
       </Swiper>
+      <View className="w-full p-5">
+        <CustomButton title="Next"></CustomButton>
+      </View>
     </SafeAreaView>
   );
 };
---
+++ components/CustomButton.tsx
@@ -0,0 +1,57 @@
+import { Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { ButtonProps } from "@/types/type";
+
+const CustomButton = ({
+  IconLeft,
+  IconRight,
+  title,
+  bgVariant,
+  textVariant,
+  className,
+}: ButtonProps) => {
+  let getBgVariant = () => {
+    switch (bgVariant) {
+      case "primary":
+        return "bg-primary-500";
+      case "secondary":
+        return "bg-secondary-500";
+      case "danger":
+        return "bg-red-500";
+      case "success":
+        return "bg-green-500";
+      case "outline":
+        return "bg-transparent border border-primary-500";
+      default:
+        return "bg-[#0286FF]";
+    }
+  };
+
+  let getTextVariant = () => {
+    switch (textVariant) {
+      case "primary":
+        return "text-black";
+      case "secondary":
+        return "text-gray-100";
+      case "danger":
+        return "text-red-100";
+      case "success":
+        return "text-green-100";
+      default:
+        return "text-white";
+    }
+  };
+  return (
+    <TouchableOpacity
+      className={`${getBgVariant()}  p-3 rounded-full shadow-md shadow-neutral-400 ${className}`}
+    >
+      {IconLeft && <IconLeft />}
+      <Text className={`${getTextVariant()} text-center text-lg font-bold`}>
+        {title}
+      </Text>
+      {IconRight && <IconRight />}
+    </TouchableOpacity>
+  );
+};
+
+export default CustomButton;
