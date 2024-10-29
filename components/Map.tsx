26fc67570c3a415932f9e2f23cb9c269a9d34410
HlaingMinThan <hlaingminthan@92@gmail.com>
2024-10-29 13:05:57 +0700

    set initial region

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,16 +1,31 @@
 import { Text } from "react-native";
 import React from "react";
 import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
+import { useLocationStore } from "@/store";
+import { calculateRegion } from "@/lib/map";
 
 const Map = () => {
-  const region = {};
+  const {
+    userLongitude,
+    userLatitude,
+    destinationLatitude,
+    destinationLongitude,
+  } = useLocationStore();
+
+  const region = calculateRegion({
+    userLatitude,
+    userLongitude,
+    destinationLatitude,
+    destinationLongitude,
+  });
+
   return (
     <MapView
       provider={PROVIDER_DEFAULT}
       className="w-full h-full rounded-2xl"
       tintColor="black"
       mapType="mutedStandard"
-      // initialRegion={region}
+      initialRegion={region}
       userInterfaceStyle="light"
     >
       <Text>Map</Text>
